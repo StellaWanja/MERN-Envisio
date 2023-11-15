f96e5d838a3dc25a072bd2dd9000fa0e803706d5
StellaWanja <stylades@gmail.com>
2023-11-15 17:45:29 +0300

    deploy

--- envisio-backend/index.js
+++ envisio-backend/index.js
@@ -23,26 +23,8 @@ app.use(express.json());
 //   allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin'],
 //   exposedHeaders: ['Content-Type', 'Access-Control-Allow-Origin']
 // };
-app.use(cors());
+app.use(cors({origin: 'https://envisio-frontend.vercel.app/'}));
 
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://envisio-frontend.vercel.app');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 //routes for apis
 app.use("/api/v2/auth", authRoutes);
 app.use("/api/v2/", patientAppRoutes);
