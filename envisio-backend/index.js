9452d6448807d20624966bda7f131afa62f13e8a
StellaWanja <stylades@gmail.com>
2023-11-15 21:39:24 +0300

    deploy vercel

--- envisio-backend/index.js
+++ envisio-backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
   next();
 });
