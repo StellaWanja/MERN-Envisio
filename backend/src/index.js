8144465f42d7a5651f7f625f5cd1dcaba770af0f
StellaWanja <stylades@gmail.com>
2024-06-06 13:03:49 +0300

    Update database configurations

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,17 +6,11 @@ import authRoutes from "./routes/authRoutes.js";
 import patientAppRoutes from "./routes/patientAppRoutes.js";
 import testResultAppRoutes from "./routes/testResultAppRoutes.js";
 import predictTestRoute from "./routes/predictTestRoute.js";
+import connectDB from "./db.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-// app.use((_req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,POST,PUT')
-
-//   next();
-// });
+dotenv.config();
 
 // app.use(cors({ credentials: true }));
 app.use(cors());
@@ -24,30 +18,14 @@ app.use(cors());
 //middleware for parsing body
 app.use(express.json());
 
-dotenv.config();
-// Middleware for handling CORS POLICY
-// const corsOpts = {
-//   origin: '*',
-//   credentials: true,
-//   methods: ['GET','POST','PUT','PATCH'],
-//   allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin'],
-//   exposedHeaders: ['Content-Type', 'Access-Control-Allow-Origin']
-// };
-
-// app.use(cors(corsOpts));
 //routes for apis
 app.use("/api/v2/auth", authRoutes);
 app.use("/api/v2/", patientAppRoutes);
 app.use("/api/v2/patient", testResultAppRoutes);
 app.use("/api/v2/predict", predictTestRoute);
 
-mongoose
-  .connect(process.env.mongoDBURL)
-  .then(() => {
-    app.listen(port);
-  })
-  .catch((error) => {
-    return { message: error.message };
-  });
+app.listen(port, async () => {
+  console.log(`App is running at http://localhost:${port}`);
+  await connectDB();
+})
 
-export default app;
